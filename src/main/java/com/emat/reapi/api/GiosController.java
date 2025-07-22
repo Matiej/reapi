69b98c0376acfd07efe65b7faf506277889d965c
Maciej Wójcik <maciek.wojcik179@gmail.com>
2025-07-22 18:27:57 +0200

    EMAT-004 | remove reponseEntity

--- src/main/java/com/emat/reapi/api/GiosController.java
+++ src/main/java/com/emat/reapi/api/GiosController.java
@@ -10,7 +10,6 @@ import com.emat.reapi.gios.port.GiosService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -23,21 +22,15 @@ public class GiosController {
     private final GiosService giosService;
 
     @GetMapping("/stations/synchronise")
-    ResponseEntity<Mono<GiosStationsDto>> synchroniseStations() {
+    Mono<GiosStationsDto> synchroniseStations() {
         log.info("GET /gios/stations/synchronise");
-        Mono<GiosStationsDto> allStations = giosService.synchronizeStations().map(GiosStations::toDto);
-        return ResponseEntity
-                .status(200)
-                .body(allStations);
+        return giosService.synchronizeStations().map(GiosStations::toDto);
     }
 
     @GetMapping("/aqindex")
-    ResponseEntity<Mono<GiosAqIndexDto>> getAqIndex(@RequestParam String stationId) {
+   Mono<GiosAqIndexDto> getAqIndex(@RequestParam String stationId) {
         log.info("GET /gios/aqindex for stationId: {} called", stationId);
-        Mono<GiosAqIndexDto> aqIndex = giosService.getAqIndex(stationId).map(GiosAqIndex::toDto);
-        return ResponseEntity
-                .status(200)
-                .body(aqIndex);
+        return giosService.getAqIndex(stationId).map(GiosAqIndex::toDto);
     }
 
     @GetMapping("/aqindex/synchronise")
